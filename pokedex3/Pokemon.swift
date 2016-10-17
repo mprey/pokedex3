b5faab290eac8a8952e08591faa61efdec0cdd5e
Mason Prey <masonprey7@gmail.com>
2016-10-17 09:50:56 -0500

    prepare Pokemon model for in depth details

--- pokedex3/Pokemon.swift
+++ pokedex3/Pokemon.swift
@@ -31,18 +31,83 @@ class Pokemon {
         return self._pokedexID
     }
     
+    var description: String {
+        if self._description == nil {
+            self._description = ""
+        }
+        return self._description!
+    }
+    
+    var type: String {
+        if self._type == nil {
+            self._type = ""
+        }
+        return self._type!
+    }
+    
+    var defense: String {
+        if self._defense == nil {
+            self._defense = ""
+        }
+        return self._defense!
+    }
+    
+    var height: String {
+        if self._height == nil {
+            self._height = ""
+        }
+        return self._height!
+    }
+    
+    var weight: String {
+        if self._weight == nil {
+            self._weight = ""
+        }
+        return self._weight!
+    }
+    
+    var attack: String {
+        if self._attack == nil {
+            self._attack = ""
+        }
+        return self._attack!
+    }
+    
+    var nextEvolutionText: String {
+        if self._nextEvolutionText == nil {
+            self._nextEvolutionText = ""
+        }
+        return self._nextEvolutionText!
+    }
+    
     init(name: String, pokeid: Int) {
         self._name = name
         self._pokedexID = pokeid
         
-        self._pokeapiURL = "\(kMPBaseURL)\(kMPPokeURL)\(self._pokedexID)!"
+        self._pokeapiURL = "\(kMPBaseURL)\(kMPPokeURL)\(self._pokedexID!)"
     }
     
     func downloadData(completed: DownloadComplete) {
         let url = URL(string: self._pokeapiURL)!
-        print(url, "\n\n\n\n\n\n\n")
         Alamofire.request(url).responseJSON { response in
-            print(response)
+            if let dict = response.result.value as? Dictionary<String, AnyObject> {
+                if let attack = dict["attack"] as? String {
+                    self._attack = attack
+                }
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let defense = dict["defense"] as? String {
+                    self._defense = defense
+                }
+            }
+            completed()
         }
     }
 }
